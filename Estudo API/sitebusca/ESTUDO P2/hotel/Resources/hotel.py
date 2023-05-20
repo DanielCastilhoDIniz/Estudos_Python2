03b19faad27f5721efa13c6f803e3284a267356f
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-05-20 01:20:59 -0300

    organizando models e resources

--- Estudo API/sitebusca/ESTUDO P2/hotel/Resources/hotel.py
+++ Estudo API/sitebusca/ESTUDO P2/hotel/Resources/hotel.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 
 hoteis = [
@@ -47,10 +48,20 @@ hoteis = [
     }
 ]
 
+
+
+
 # Define uma classe Hoteis que herda da classe Resource
 
 
 class Hoteis(Resource):
+
+    # Método GET para a classe Hoteis
+    def get(self):
+        return {'hoteis': hoteis}
+
+
+class Hotel(Resource):
 
     argumento = reqparse.RequestParser()
     argumento.add_argument('nome')
@@ -58,18 +69,11 @@ class Hoteis(Resource):
     argumento.add_argument('diarias')
     argumento.add_argument('cidade')
 
-# Método GET para a classe Hoteis
-    def get(self):
-        return {'hoteis': hoteis}
-
-
-class Hotel(Resource):
     # Método para encontrar um hotel pelo ID
     def find_hotel(hotel_id):
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
-
         return None
 
     def get(self, hotel_id):
@@ -80,24 +84,29 @@ class Hotel(Resource):
 
     def post(self, hotel_id):
 
-        dados = Hotel.argumento.parse_args()  # Analisa os argumentos da requisição usando o RequestParser
-        novo_hotel = {
-            "hotel_id": hotel_id,
-            "nome": dados['nome'],
-            "estrelas": dados['estrelas'],
-            "diarias": dados['diarias'],
-            "cidade": dados['cidade']
-        }
+        # Analisa os argumentos da requisição usando o RequestParser
+        dados = Hotel.argumento.parse_args()
+        hotel_objeto = HotelModel(hotel_id,**dados)
+        novo_hotel = hotel_objeto.json()
         hoteis.append(novo_hotel)
         return novo_hotel, 200
 
     def put(self, hotel_id):
         dados = Hotel.argumento.parse_args()
+        hotel_objeto = HotelModel(hotel_id,**dados)
+        novo_hotel = hotel_objeto.json()
+        # novo_hotel = {"hotel_id": hotel_id, **dados}
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
-            hotel.update(novo_hotel) # Atualiza as informações do hotel com os novos dados
+            # Atualiza as informações do hotel com os novos dados
+            hotel.update(novo_hotel)
+            return novo_hotel, 200  # ok
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201  # created
 
         pass
 
     def delete(self, hotel_id):
-        pass
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message': 'Hotel deleted'}
