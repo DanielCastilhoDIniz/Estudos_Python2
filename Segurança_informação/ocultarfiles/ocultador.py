bef180220aaa902be08cc155df4ed5dc2339381f
Daniel Castilho <danielcastilhodiniz@gmail.com>
2023-06-25 14:41:26 -0300

    update estudos

---
+++ Segurança_informação/ocultarfiles/ocultador.py
@@ -0,0 +1,12 @@
+import ctypes
+
+atributo_ocultar = 0x02
+
+retorno = ctypes.windll.kernel32.SetFileAttributesW('ocultar.txt', atributo_ocultar)
+
+if retorno:
+    print('Arquivo ocultado')
+
+else:
+    print('Arquivo não Ocultado')
+
